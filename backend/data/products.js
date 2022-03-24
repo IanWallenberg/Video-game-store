9ed55140fddfe2916e8f67f69c3170b57e08c0ff
IanWallenberg <90833604+IanWallenberg@users.noreply.github.com>
2022-03-24 12:00:38 +0100

    Adding the backend folders, front end folders, react start up, video game data, plus necessary npm etc

---
+++ backend/data/products.js
@@ -0,0 +1,108 @@
+const products = [
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game! In good quality",
+        price: 299,
+        countInStock: 2,
+    },
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+
+    {
+        name: "Crash Bandicoot 1",
+        imageUrl: "",
+        description:
+        "The classic game!",
+        price: 299,
+        countInStock: 2,
+    },
+]
